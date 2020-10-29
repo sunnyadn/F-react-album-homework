4ce6905817a27ba820d5ffebc5a1a8035d4f7fff
Sunny Yang <sunnyadn@foxmail.com>
2020-10-29 15:53:44 +0800

    Implement User Info aside

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 import './UserInfo.scss';
 
+function renderTableRow(head, data, link) {
+  return (
+    <tr>
+      <th>{head}</th>
+      <td>
+        <a href={link}>{data}</a>
+      </td>
+    </tr>
+  );
+}
+
 class UserInfo extends Component {
+  renderTable() {
+    const { email, phone, website, company } = this.props.info;
+    return (
+      <table>
+        {[
+          renderTableRow('Email', email, `mailto:${  email}`),
+          renderTableRow('Phone', phone, `tel:${  phone}`),
+          renderTableRow('Website', website, `http://${  website}`),
+          renderTableRow('Company', company.name),
+        ]}
+      </table>
+    );
+  }
+
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    const { name } = this.props.info;
+    return (
+      <aside className="UserInfo">
+        <h1>{name}</h1>
+        <h2>Contact Me @</h2>
+        {this.renderTable()}
+      </aside>
+    );
   }
 }
 
