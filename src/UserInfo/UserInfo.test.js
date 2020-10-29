8354eaa29c3615b793425851775683e4a15efd7f
Sunny Yang <sunnyadn@foxmail.com>
2020-10-29 15:30:39 +0800

    test if user info displays information correctly

--- src/UserInfo/UserInfo.test.js
+++ src/UserInfo/UserInfo.test.js
@@ -4,9 +4,24 @@ import { shallow } from 'enzyme';
 import UserInfo from './UserInfo';
 
 describe('<UserInfo />', () => {
-  it('should display name', () => {
-    const userInfo = shallow(<UserInfo />);
+  it('should display information', () => {
+    const info = {
+      name: 'Sunny Yang',
+      email: 'guang.yang@thoughtworks.com',
+      phone: '1234567890',
+      website: 'guang.yang.thoughtworks.com',
+      company: { name: 'ThoughtWorks' },
+    };
 
-    expect(userInfo.text()).toContain('Leanne Graham');
+    const userInfo = shallow(<UserInfo info={info} />);
+
+    const text = userInfo.text();
+
+    expect(text).toContain(info.name);
+    expect(text).toContain('Contact Me @');
+    expect(text).toContain(`Email${  info.email}`);
+    expect(text).toContain(`Phone${  info.phone}`);
+    expect(text).toContain(`Website${  info.website}`);
+    expect(text).toContain(`Company${  info.company.name}`);
   });
 });
