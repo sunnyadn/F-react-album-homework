6ac07221594c97876dccf805657dd1ba862f6886
Sunny Yang <sunnyadn@foxmail.com>
2020-10-30 17:26:58 +0800

    test for showing image in first album as default

--- src/Albums/Albums.test.js
+++ src/Albums/Albums.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { fetchAlbums } from '../apiUtil';
+import { fetchAlbums, fetchPhotos } from '../apiUtil';
 import Albums from './Albums';
 
 jest.mock('../apiUtil');
@@ -8,27 +8,74 @@ jest.mock('../apiUtil');
 const flushPromises = () => new Promise(setImmediate);
 
 describe('<Albums />', () => {
-  it('should display album titles', async () => {
-    const data = [
-      {
-        id: '1',
-        title: 'quidem molestiae enim',
-      },
-      {
-        id: '2',
-        title: 'sunt qui excepturi placeat culpa',
-      },
-    ];
+  const albums = [
+    {
+      id: '1',
+      title: 'quidem molestiae enim',
+    },
+    {
+      id: '2',
+      title: 'sunt qui excepturi placeat culpa',
+    },
+  ];
 
-    fetchAlbums.mockResolvedValue(data);
+  const photos = [
+    {
+      albumId: 1,
+      id: 1,
+      title: 'accusamus beatae ad facilis cum similique qui sunt',
+      url: 'https://via.placeholder.com/600/92c952',
+      thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+    },
+    {
+      albumId: 1,
+      id: 2,
+      title: 'reprehenderit est deserunt velit ipsam',
+      url: 'https://via.placeholder.com/600/771796',
+      thumbnailUrl: 'https://via.placeholder.com/150/771796',
+    },
+    {
+      albumId: 1,
+      id: 3,
+      title: 'officia porro iure quia iusto qui ipsa ut modi',
+      url: 'https://via.placeholder.com/600/24f355',
+      thumbnailUrl: 'https://via.placeholder.com/150/24f355',
+    },
+    {
+      albumId: 1,
+      id: 4,
+      title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+      url: 'https://via.placeholder.com/600/d32776',
+      thumbnailUrl: 'https://via.placeholder.com/150/d32776',
+    },
+    {
+      albumId: 1,
+      id: 5,
+      title: 'natus nisi omnis corporis facere molestiae rerum in',
+      url: 'https://via.placeholder.com/600/f66b97',
+      thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
+    },
+  ];
 
-    const wrapper = shallow(<Albums />);
+  fetchAlbums.mockResolvedValue(albums);
+  fetchPhotos.mockResolvedValue(photos);
+
+  let wrapper;
+
+  beforeAll(async () => {
+    wrapper = shallow(<Albums />);
     await flushPromises();
     wrapper.update();
+  });
 
+  it('should display album titles', () => {
     const text = wrapper.text();
 
-    expect(text).toContain(data[0].title);
-    expect(text).toContain(data[1].title);
+    expect(text).toContain(albums[0].title);
+    expect(text).toContain(albums[1].title);
+  });
+
+  it('should show the first three photos of the first album', async () => {
+    expect(wrapper.find('img')).toHaveLength(3);
   });
 });
