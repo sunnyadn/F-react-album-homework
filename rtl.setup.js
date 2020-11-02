3174cbc839c808ce50d8726ea10737e67e9cd3b6
Sunny Yang <sunnyadn@foxmail.com>
2020-11-02 15:24:08 +0800

    Revert "setup jest for unit test"
    This reverts commit 0583cd25077b53f573a69a9624cf565a8f7c1277.

--- rtl.setup.js
+++ rtl.setup.js
@@ -1,7 +1,2 @@
 // See https://github.com/kentcdodds/react-testing-library#global-config
 import '@testing-library/jest-dom/extend-expect';
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
